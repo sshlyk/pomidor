87dc6dfb41be068b71c666f7131e615fcfa10b8c
Sergey Shlykovich <sshlyk@gmail.com>
2025-04-26 11:27:31 -0700

    First commit

---
+++ pomidor/pomidorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomidorApp.swift
+//  pomidor
+//
+//  Created by Mr S on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct pomidorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomidorTests/pomidorTests.swift
@@ -0,0 +1,16 @@
+//
+//  pomidorTests.swift
+//  pomidorTests
+//
+//  Created by Mr S on 4/26/25.
+//
+
+import Testing
+
+struct pomidorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
